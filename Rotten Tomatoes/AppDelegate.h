b9a3e989d15bba09f96f026f88bd44bc47db3c0f
Kittipat Virochsiri <kittipat@thisbluepla.net>
2015-02-03 23:31:52 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Kittipat Virochsiri on 2/3/15.
+//  Copyright (c) 2015 Kittipat Virochsiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Kittipat Virochsiri on 2/3/15.
+//  Copyright (c) 2015 Kittipat Virochsiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
